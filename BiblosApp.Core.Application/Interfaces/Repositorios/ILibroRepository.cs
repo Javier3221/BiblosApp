b492a85b6b1f40117c5720465a973fff1c7132a8
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 18:24:11 -0400

    Agregar repositorio de Libro

---
+++ BiblosApp.Core.Application/Interfaces/Repositorios/ILibroRepository.cs
@@ -0,0 +1,13 @@
+﻿using BiblosApp.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.Interfaces.Repositorios
+{
+    public interface ILibroRepository : IGenericRepository<Libro>
+    {
+    }
+}
---
+++ BiblosApp.Infrastructure.Persistence/Repositories/LibroRepository.cs
@@ -0,0 +1,20 @@
+﻿using BiblosApp.Core.Application.Interfaces.Repositorios;
+using BiblosApp.Core.Domain.Entities;
+using BiblosApp.Infrastructure.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Infrastructure.Persistence.Repositories
+{
+    internal class LibroRepository : GenericRepository<Libro>, ILibroRepository
+    {
+        private ApplicationContext _dbContext;
+        public LibroRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
