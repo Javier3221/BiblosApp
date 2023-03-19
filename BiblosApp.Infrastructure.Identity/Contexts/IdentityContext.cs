d9faea064fd938647cd26f10fc0ff0012a26848b
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 18:27:10 -0400

    Crear Esquema Identity

---
+++ BiblosApp.Infrastructure.Identity/Contexts/IdentityContext.cs
@@ -0,0 +1,47 @@
+﻿using BiblosApp.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Infrastructure.Identity.Contexts
+{
+    public class IdentityContext : IdentityDbContext<AppUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> opciones) : base(opciones) 
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("Identity");
+
+            builder.Entity<AppUser>(entity =>
+            {
+                entity.ToTable("Usuario");
+            });
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable("Roles");
+            });
+
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("RolesUsuario");
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("LoginUsuario");
+            });
+        }
+    }
+}
