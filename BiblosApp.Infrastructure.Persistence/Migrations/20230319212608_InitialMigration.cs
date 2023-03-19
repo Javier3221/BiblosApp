e5afcae7fbc4b7fc5a7a1e1f944387ac9d753341
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 18:28:03 -0400

    Crear BiblosAppDB

---
+++ BiblosApp.Infrastructure.Persistence/Migrations/20230319212608_InitialMigration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BiblosApp.Infrastructure.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Num_publicaciones = table.Column<int>(type: "int", nullable: false),
+                    CreadoPor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaCreado = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Libro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Id_autor = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<double>(type: "float", nullable: false),
+                    Num_paginas = table.Column<int>(type: "int", nullable: false),
+                    Link_portada = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cantidad_inventario = table.Column<int>(type: "int", nullable: false),
+                    CreadoPor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaCreado = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libro", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Libro_Autor_Id_autor",
+                        column: x => x.Id_autor,
+                        principalTable: "Autor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libro_Id_autor",
+                table: "Libro",
+                column: "Id_autor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libro");
+
+            migrationBuilder.DropTable(
+                name: "Autor");
+        }
+    }
+}
