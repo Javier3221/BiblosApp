07138e0c19f51b5e758e44e2de71a6ef5420fc0e
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 18:22:51 -0400

    Agregar seeds al arranque

---
+++ BiblosApp.Infrastructure.Identity/Seeds/RolesPorDefecto.cs
@@ -0,0 +1,20 @@
+﻿using BiblosApp.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiblosApp.Core.Application.Enums;
+
+namespace BiblosApp.Infrastructure.Identity.Seeds
+{
+    public static class RolesPorDefecto
+    {
+        public static async Task SeedAsync(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(RolesUsuario.Administrativo.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(RolesUsuario.Cliente.ToString()));
+        }
+    }
+}
--- BiblosApp/Program.cs
+++ BiblosApp/Program.cs
@@ -1,5 +1,9 @@
+using BiblosApp.Infrastructure.Identity.Entities;
+using BiblosApp.Infrastructure.Identity.Seeds;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,9 +15,28 @@ namespace BiblosApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    await RolesPorDefecto.SeedAsync(userManager, roleManager);
+                    await AdminPorDefecto.SeedAsync(userManager, roleManager);
+                } 
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
