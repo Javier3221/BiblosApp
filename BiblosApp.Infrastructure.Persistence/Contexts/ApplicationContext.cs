daeeb29ed15fc1e60bda68de65d7ba00e68aca9c
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 16:30:36 -0400

    Crear DbContext

---
+++ BiblosApp.Infrastructure.Persistence/Contexts/ApplicationContext.cs
@@ -0,0 +1,72 @@
+﻿using BiblosApp.Core.Domain.Common;
+using BiblosApp.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Infrastructure.Persistence.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<EntidadBaseAuditable>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.FechaCreado = DateTime.Now;
+                        entry.Entity.CreadoPor = "BiblosApp Developer Team";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<Libro> Libro { get; set; }
+        public DbSet<Autor> Autor { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region Tablas
+
+            modelBuilder.Entity<Libro>()
+                .ToTable("Libro");
+
+            modelBuilder.Entity<Autor>()
+                .ToTable("Autor");
+
+            #endregion
+
+            #region Llaves Primarias
+
+            modelBuilder.Entity<Libro>()
+                .HasKey(k => k.Id);
+
+            modelBuilder.Entity<Autor>()
+                .HasKey(k => k.Id);
+
+            #endregion
+
+            #region Relaciones
+
+            modelBuilder.Entity<Autor>()
+                .HasMany<Libro>(g => g.Libros)
+                .WithOne(s => s.Autor)
+                .HasForeignKey(k => k.Id_autor)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            #endregion
+        }
+    }
+}
---
+++ BiblosApp.Infrastructure.Persistence/RegistrarSerivicios.cs
@@ -0,0 +1,26 @@
+﻿using BiblosApp.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Infrastructure.Persistence
+{
+    public static class RegistrarSerivicios
+    {
+        public static void AddPersistenceInfrastructure(this IServiceCollection servicios, IConfiguration configuracion)
+        {
+            #region Context
+
+            servicios.AddDbContext<ApplicationContext>(opciones =>
+            opciones.UseSqlServer(configuracion.GetConnectionString("ConexionPorDefecto"),
+            m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+
+            #endregion
+        }
+    }
+}
