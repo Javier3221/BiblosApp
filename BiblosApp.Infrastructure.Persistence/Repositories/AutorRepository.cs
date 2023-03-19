9a8f71b23a098ae1218a039fd88d4aad6708014b
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 18:23:43 -0400

    Agregar repositorio de autor

---
+++ BiblosApp.Infrastructure.Persistence/Repositories/AutorRepository.cs
@@ -0,0 +1,23 @@
+﻿using BiblosApp.Core.Application.Interfaces.Repositorios;
+using BiblosApp.Core.Domain.Entities;
+using BiblosApp.Infrastructure.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Infrastructure.Persistence.Repositories
+{
+    public class AutorRepository : GenericRepository<Autor>, IAutorRepository
+    {
+        private ApplicationContext _dbContext;
+
+        public AutorRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+    }
+}
