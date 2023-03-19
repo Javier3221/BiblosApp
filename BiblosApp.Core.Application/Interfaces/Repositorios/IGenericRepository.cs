abb8e0fd4a8caf78b3c59b459d2793fb98dde1c4
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 18:23:17 -0400

    Agregar repositorio generico

---
+++ BiblosApp.Core.Application/Interfaces/Repositorios/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.Interfaces.Repositorios
+{
+    public interface IGenericRepository<Entity>
+    {
+        Task<Entity> AgregarAsync(Entity entity);
+        Task<Entity> EditarAsync(Entity entity, int id);
+        Task EliminarAsync(Entity entity);
+        Task<List<Entity>> ObtenerEntidades();
+        Task<List<Entity>> GetAllWithIncludeAsync(List<string> properties);
+        Task<Entity> ObtenerPorIdAsync(int id);
+    }
+}
---
+++ BiblosApp.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using BiblosApp.Core.Application.Interfaces.Repositorios;
+using BiblosApp.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<Entity> : IGenericRepository<Entity> where Entity : class
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public GenericRepository(ApplicationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<Entity> AgregarAsync(Entity entity)
+        {
+            await _dbContext.Set<Entity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task<Entity> EditarAsync(Entity entity, int id)
+        {
+            var entry = await _dbContext.Set<Entity>().FindAsync(id);
+            _dbContext.Entry(entry).CurrentValues.SetValues(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task EliminarAsync(Entity entity)
+        {
+            _dbContext.Set<Entity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<List<Entity>> ObtenerEntidades()
+        {
+            return await _dbContext.Set<Entity>().ToListAsync();
+        }
+
+        public virtual async Task<List<Entity>> GetAllWithIncludeAsync(List<string> properties)
+        {
+            var query = _dbContext.Set<Entity>().AsQueryable();
+
+            foreach (string property in properties)
+            {
+                query = query.Include(property);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public virtual async Task<Entity> ObtenerPorIdAsync(int id)
+        {
+            return await _dbContext.Set<Entity>().FindAsync(id);
+        }
+    }
+}
