193444f70d398d22e9685a210212c61e90f10e77
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 18:27:41 -0400

    Registrar identity al arranque

---
+++ BiblosApp.Infrastructure.Identity/RegistrarServicios.cs
@@ -0,0 +1,49 @@
+﻿using BiblosApp.Infrastructure.Identity.Contexts;
+using BiblosApp.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Infrastructure.Identity
+{
+    public static class RegistrarServicios
+    {
+        public static void AddIdentityInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Context
+
+            services.AddDbContext<IdentityContext>(options =>
+            {
+                options.EnableSensitiveDataLogging();
+                options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"),
+                m => m.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName));
+            });
+
+            #endregion
+
+            #region IdentityConfiguration
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/User/LogIn";
+                options.AccessDeniedPath = "/User/AccessDenied";
+            });
+
+            services.AddAuthentication();
+            #endregion
+
+            #region Servicios
+            //services.AddTransient<IAccountService, AccountService>();
+            #endregion
+        }
+    }
+}
