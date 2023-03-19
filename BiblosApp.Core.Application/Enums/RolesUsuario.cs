ad4659fa665979a9b76997b6fa7723c473d9e191
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 18:25:47 -0400

    Agregar Enum de roles de usuario

---
+++ BiblosApp.Core.Application/Enums/RolesUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.Enums
+{
+    public enum RolesUsuario
+    {
+        Administrativo = 1,
+        Cliente
+    }
+}
