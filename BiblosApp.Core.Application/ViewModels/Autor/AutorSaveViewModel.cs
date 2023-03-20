94161840d89b6826f173a4b7ba2d949876573993
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 21:34:10 -0400

    Agregar view models para Autor

---
+++ BiblosApp.Core.Application/ViewModels/Autor/AutorSaveViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.ViewModels.Autor
+{
+    public class AutorSaveViewModel
+    {
+        [Required(ErrorMessage = "Debe especificar un nombre")]
+        [DataType (DataType.Text)]
+        public string Nombre { get; set; }
+
+        [DataType(DataType.Text)]
+        public int Num_publicaciones { get; set; }
+    }
+}
