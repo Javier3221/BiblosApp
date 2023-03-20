b42438d4f9869516852be249fbdd9e861d5ad507
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 23:13:18 -0400

    Crear DTOs para registro de usuario

---
+++ BiblosApp.Core.Application/DTOs/Account/RegisterRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.DTOs.Account
+{
+    public class RegisterRequest
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Contrasena { get; set; }
+        public string ConfirmarContrasena { get; set; }
+
+    }
+}
