c3677326c490e8b4a37e229e592efa666a015a8a
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 21:45:24 -0400

    Agregar servicio de Libro

---
+++ BiblosApp.Core.Application/Interfaces/Servicios/ILibroService.cs
@@ -0,0 +1,14 @@
+﻿using BiblosApp.Core.Application.ViewModels.Libro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.Interfaces.Servicios
+{
+    public interface ILibroService : IGenericService<LibroSaveViewModel, LibroViewModel>
+    {
+            
+    }
+}
---
+++ BiblosApp.Core.Application/Services/LibroService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using BiblosApp.Core.Application.Interfaces.Repositorios;
+using BiblosApp.Core.Application.Interfaces.Servicios;
+using BiblosApp.Core.Application.ViewModels.Libro;
+using BiblosApp.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.Services
+{
+    public class LibroService : GenericService<LibroSaveViewModel, LibroViewModel, Libro>, ILibroService
+    {
+        private readonly ILibroRepository _libroRepository;
+        private readonly IMapper _mapper;
+
+        public LibroService(ILibroRepository libroRepository, IMapper mapper) : base(libroRepository, mapper) 
+        {
+            _libroRepository = libroRepository;
+            _mapper = mapper;
+        }
+    }
+}
