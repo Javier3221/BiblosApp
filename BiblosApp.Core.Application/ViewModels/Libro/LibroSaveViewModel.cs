0aa200af0358e9e3498da658cf316e5df83ecebb
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 21:41:58 -0400

    Agregar view models para Libro

---
+++ BiblosApp.Core.Application/ViewModels/Libro/LibroSaveViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.ViewModels.Libro
+{
+    public class LibroSaveViewModel
+    {
+        [Required(ErrorMessage = "Debe especificar el nombre del libro")]
+        [DataType(DataType.Text)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe especificar el autor del libro")]
+        public int Id_autor { get; set; }
+
+        [Required(ErrorMessage = "Debe especificar el precio del libro")]
+        [DataType(DataType.Currency)]
+        public Double Precio { get; set; }
+
+        [Required(ErrorMessage = "Debe especificar la cantidad de paginas del libro")]
+        public int Num_paginas { get; set; }
+
+        [Required(ErrorMessage = "Debe incluir una portada para el libro")]
+        [DataType(DataType.ImageUrl)]
+        public string Link_portada { get; set; }
+
+        [Required(ErrorMessage = "Debe especificar la cantidad del libro en inventario")]
+        public int Cantidad_inventario { get; set; }
+    }
+}
---
+++ BiblosApp.Core.Application/ViewModels/Libro/LibroViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BiblosApp.Core.Application.ViewModels.Autor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.ViewModels.Libro
+{
+    public class LibroViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public Double Precio { get; set; }
+        public int Num_paginas { get; set; }
+        public string Link_portada { get; set; }
+        public int Cantidad_inventario { get; set; }
+
+        public AutorViewModel Autor { get; set; }
+    }
+}
