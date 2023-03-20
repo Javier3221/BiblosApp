f771f7444bc3136b5082a5a59037ebe56df53e27
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 21:41:13 -0400

    View Models para autor

---
+++ BiblosApp.Core.Application/ViewModels/Autor/AutorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BiblosApp.Core.Application.ViewModels.Libro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.ViewModels.Autor
+{
+    public class AutorViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Num_publicaciones { get; set; }
+
+        public List<LibroViewModel> Libros { get; set; } = new();
+    }
+}
