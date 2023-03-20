87ac11766451f24906a057e289696425face0bcb
Javier Perez <javierpdc13@hotmail.com>
2023-03-19 21:43:32 -0400

    Agregar servicios de Autor

---
+++ BiblosApp.Core.Application/Interfaces/Servicios/IAutorService.cs
@@ -0,0 +1,13 @@
+﻿using BiblosApp.Core.Application.ViewModels.Autor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.Interfaces.Servicios
+{
+    public interface IAutorService : IGenericService<AutorSaveViewModel, AutorViewModel>
+    {
+    }
+}
---
+++ BiblosApp.Core.Application/Services/AutorService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using BiblosApp.Core.Application.Interfaces.Repositorios;
+using BiblosApp.Core.Application.Interfaces.Servicios;
+using BiblosApp.Core.Application.ViewModels.Autor;
+using BiblosApp.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Core.Application.Services
+{
+    public class AutorService : GenericService<AutorSaveViewModel, AutorViewModel, Autor>, IAutorService
+    {
+        private readonly IAutorRepository _autorRepository;
+        private readonly IMapper _mapper;
+
+        public AutorService(IAutorRepository autorRepository, IMapper mapper) : base(autorRepository, mapper)
+        {
+            _autorRepository = autorRepository;
+            _mapper = mapper;
+        }
+    }
+}
