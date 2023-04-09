c53cab89c77d22b7a3b56a585e270d7a6cca9d83
Javier Perez <javierpdc13@hotmail.com>
2023-04-09 11:16:05 -0400

    Agregar controlador para Admins

---
+++ BiblosApp/Controllers/AdminController.cs
@@ -0,0 +1,76 @@
+﻿using BiblosApp.Core.Application.Interfaces.Servicios;
+using BiblosApp.Core.Application.ViewModels.Libro;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BiblosApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILibroService _libroService;
+        private readonly IAutorService _autorService;
+        public AdminController(ILibroService libroService, IAutorService autorService) 
+        {
+            _libroService = libroService;
+            _autorService = autorService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _libroService.ObtenerIncludes());
+        }
+
+        public async Task<IActionResult> CrearLibro()
+        {
+            ViewData["isEdit"] = false;
+            LibroSaveViewModel saveViewModel = new();
+            saveViewModel.ListadoAutores = await _autorService.ObtenerTodos();
+            return View("SaveLibro", saveViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearLibro(LibroSaveViewModel sVM)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                ViewData["isEdit"] = false;
+                LibroSaveViewModel saveViewModel = new();
+                saveViewModel.ListadoAutores = await _autorService.ObtenerTodos();
+                return View("SaveLibro", saveViewModel);
+            }
+
+            await _libroService.Agregar(sVM);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> EditarLibro(int id)
+        {
+            ViewData["isEdit"] = true;
+            LibroSaveViewModel saveViewModel = await _libroService.ObtenerPorIdSaveViewModel(id);
+            saveViewModel.ListadoAutores = await _autorService.ObtenerTodos();
+            return View("SaveLibro", saveViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditarLibro(LibroSaveViewModel sVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["isEdit"] = true;
+                LibroSaveViewModel saveViewModel = await _libroService.ObtenerPorIdSaveViewModel(sVM.Id);
+                saveViewModel.ListadoAutores = await _autorService.ObtenerTodos();
+                return View("SaveLibro", saveViewModel);
+            }
+
+            await _libroService.Editar(sVM, sVM.Id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EliminarLibro(int id)
+        {
+            await _libroService.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
